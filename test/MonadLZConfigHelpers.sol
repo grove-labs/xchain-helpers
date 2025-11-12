9a72edd1dc2c31b845a82ee79ed14864c97dc400
Bartek Rutkowski <barrutko.eth@gmail.com>
2025-11-12 12:48:05 -0500

    refactor: minimize additional setup

--- test/MonadLZConfigHelpers.sol
+++ test/MonadLZConfigHelpers.sol
@@ -10,7 +10,7 @@ import { Domain, DomainHelpers } from "src/testing/Domain.sol";
 /**
  * @title MonadLZConfigHelpers
  * @notice TEMPORARY WORKAROUND for incomplete Monad LayerZero deployment
- * @dev This helper configures working DVNs and Executors for the Monad route,
+ * @dev This helper configures working DVNs for the Monad route,
  *      bypassing the placeholder "deadDVN" contracts that are currently deployed.
  *
  *      ISSUE: Monad's LayerZero endpoints have placeholder DVNs configured:
@@ -34,22 +34,6 @@ struct UlnConfig {
     address[] optionalDVNs;
 }
 
-struct ExecutorConfig {
-    uint32 maxMessageSize;
-    address executor;
-}
-
-struct SetConfigParam {
-    uint32 eid;
-    uint32 configType;
-    bytes config;
-}
-
-struct SetDefaultExecutorConfigParam {
-    uint32 eid;
-    ExecutorConfig config;
-}
-
 struct SetDefaultUlnConfigParam {
     uint32 eid;
     UlnConfig config;
@@ -60,7 +44,6 @@ interface ILayerZeroEndpointV2Admin {
 }
 
 interface ISendLibAdmin {
-    function setDefaultExecutorConfigs(SetDefaultExecutorConfigParam[] calldata _params) external;
     function setDefaultUlnConfigs(SetDefaultUlnConfigParam[] calldata _params) external;
 }
 
@@ -78,12 +61,8 @@ library MonadLZConfigHelpers {
     address private constant ETHEREUM_DVN = 0x589dEDbD617e0CBcB916A9223F4d1300c294236b;  // Base/Plasma DVN
     address private constant MONAD_DVN    = 0x282b3386571f7f794450d5789911a9804FA346b4;  // LayerZero Labs DVN
 
-    // Executor Addresses
-    address private constant ETHEREUM_EXECUTOR = 0x173272739Bd7Aa6e4e214714048a9fE699453059;
-    address private constant MONAD_EXECUTOR    = 0x4208D6E27538189bB48E603D6123A94b8Abe0A0b;
-
     /**
-     * @notice Configure working DVNs and Executors for Monad↔Ethereum route
+     * @notice Configure working DVNs for Monad↔Ethereum route
      * @dev This function ONLY patches the Monad route's incomplete deployment.
      *      It sets default configurations that all OApps will inherit.
      * @param ethereumFork The Ethereum fork/domain
@@ -98,35 +77,31 @@ library MonadLZConfigHelpers {
             endpoint  : LZForwarder.ENDPOINT_ETHEREUM,
             admin     : ETHEREUM_ADMIN,
             remoteEid : LZForwarder.ENDPOINT_ID_MONAD,
-            dvn       : ETHEREUM_DVN,
-            executor  : ETHEREUM_EXECUTOR
+            dvn       : ETHEREUM_DVN
         });
         _configureDirection({
             fork      : monadFork,
             endpoint  : LZForwarder.ENDPOINT_MONAD,
             admin     : MONAD_ADMIN,
             remoteEid : LZForwarder.ENDPOINT_ID_ETHEREUM,
-            dvn       : MONAD_DVN,
-            executor  : MONAD_EXECUTOR
+            dvn       : MONAD_DVN
         });
     }
 
     /**
-     * @notice Configures default DVNs and Executors for a given source/target direction.
+     * @notice Configures default DVNs for a given source/target direction.
      * @param fork Domain of the network to operate on (source side)
      * @param endpoint Address of the LayerZero endpoint whose send library will be mutated (on 'fork')
      * @param admin Address to use for vm.prank and permissions
      * @param remoteEid The remote endpoint id this config will target
      * @param dvn Address of the working DVN for this direction
-     * @param executor Address of the working Executor for this direction
      */
     function _configureDirection(
         Domain  memory fork,
         address endpoint,
         address admin,
         uint32  remoteEid,
-        address dvn,
-        address executor
+        address dvn
     ) private {
         fork.selectFork();
 
@@ -151,19 +126,6 @@ library MonadLZConfigHelpers {
 
         vm.prank(admin);
         ISendLibAdmin(sendLib).setDefaultUlnConfigs(ulnParams);
-
-        // Set default Executor config
-        SetDefaultExecutorConfigParam[] memory execParams = new SetDefaultExecutorConfigParam[](1);
-        execParams[0] = SetDefaultExecutorConfigParam({
-            eid    : remoteEid,
-            config : ExecutorConfig({
-                maxMessageSize : 10000,
-                executor       : executor
-            })
-        });
-
-        vm.prank(admin);
-        ISendLibAdmin(sendLib).setDefaultExecutorConfigs(execParams);
     }
 
 }
